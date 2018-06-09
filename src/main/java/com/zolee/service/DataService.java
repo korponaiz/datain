deda4b9cdef7030828a72d18e8962cbec2576569
korponaiz <korponaiz@gmail.com>
2018-06-09 17:55:16 +0200

    Create MainUI.java and add @Service annotation to DataService.java

--- src/main/java/com/zolee/service/DataService.java
+++ src/main/java/com/zolee/service/DataService.java
@@ -3,10 +3,12 @@ package com.zolee.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.zolee.domain.Data;
 import com.zolee.repository.DataRepository;
 
+@Service
 public class DataService {
 
 	private DataRepository dataRepository;
